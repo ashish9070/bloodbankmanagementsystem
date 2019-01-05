79bb44da24806c61375dc05c6ef12f1b30115e51
Gnaneswar <gxg170000@utdallas.edu>
2019-01-05 17:56:05 -0600

    Add files via upload

---
+++ BMS.Exceptions/ConnectedDalException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMS.Exceptions
+{
+    [Serializable]
+    public class ConnectedDalException : Exception
+    {
+        public ConnectedDalException() { }
+        public ConnectedDalException(string message) : base(message) { }
+        public ConnectedDalException(string message, Exception inner) : base(message, inner) { }
+        protected ConnectedDalException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context)
+            : base(info, context) { }
+    }
+}
